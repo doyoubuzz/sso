e7416de685fb1cddbf4140cb6146607137a3841b
antoiner <antoiner90@hotmail.com>
2013-09-13 15:18:22 +0200

    Adding the CID parameter in doc (for multi-URL SSO)

--- dyb.php
+++ dyb.php
@@ -2,10 +2,13 @@
 $_SESSION['timestamp'] = isset( $_GET['timestamp'] ) ? $_GET['timestamp'] : '';
 
 $slug = 'battlestar';							 		            // Your slug		
+$cid = 'mycid';                                                      // If SSO with severals URLs is enabled
+
 $user = array( 'id'        => 'kara-thrace',					// The user id on your system
 			   'email'     => 'kara.thrace@doyoubuzz.com',      // User email
 			   'firstname' => 'Kara',			 	            // User first name
-			   'lastname'  => 'Thrace'				 	        // User last name
+			   'lastname'  => 'Thrace',				 	        // User last name
+			   'user_type' => '3'				 	        // User last name
 			  );
 $secret_key = 'IBebIUWBAah7EiQ';				 	            // SSO Secret Key (in your BO > Settings > General Settings > SSO)				    
 $groups = array('pilote', 'viper');                      	    // Array of the groups id (on your system) the user belongs to
@@ -20,12 +23,13 @@ if ( isset($_GET['timestamp']) ) {
 		$secret_key,
 		$user['firstname'],
 		$user['lastname'],
-		$groups
+		$groups,
+		$user['user_type']
 	);  
 	
 }
 else {
-	echo '<a href="http://showcase.doyoubuzz.com/p/fr/'.$slug.'/sso">Connexion</a>';		
+	echo '<a href="http://showcase.doyoubuzz.com/p/fr/'.$slug.'/sso?cid='.$cid.'">Connexion</a>';		
 }
 
 ?>
